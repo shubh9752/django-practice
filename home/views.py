b1252dc416027b233eff10bf1b77e6df168c400b
shubh9752 <shubhamkumar3.sk@gmail.com>
2024-04-29 04:44:24 -0500

    http done

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>this is my home </h1>')
+def success_page(req):
+    return HttpResponse('<h1>this is a success page </h1>')
+
 
 # Create your views here.
